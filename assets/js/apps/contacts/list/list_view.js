ca78c1225fcef2db546632d22f0bd80475aef5a5
Blurredbits <mmorris00@mac.com>
2014-04-13 16:39:29 -0600

    Refactored into js modules

---
+++ assets/js/apps/contacts/list/list_view.js
@@ -0,0 +1,23 @@
+ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
+  List.Contact = Marionette.ItemView.extend({
+    tagName: "tr",
+    template: "#contact-list-item",
+
+    events: {
+      "click": "highlightName"
+    },
+
+    highlightName: function(e){
+      e.preventDefault();
+      this.$el.toggleClass("warning");
+    }
+  });
+
+  List.Contacts = Marionette.CompositeView.extend({
+    tagName: "table",
+    className: "table table-hover",
+    template: "#contact-list",
+    itemView: List.Contact,
+    itemViewContainer: "tbody"
+  });
+})
